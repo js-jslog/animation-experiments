44bc8c1c94abba9fd997f1f20fdd4c1770d338f2
js-jslog <jhs4jbs@hotmail.co.uk>
2020-06-23 17:01:49 +0000

    Split off a SimplePeekScroll

---
+++ src/SimplePeekScroll/index.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react"
+import { Button, StyleSheet, View } from "react-native"
+import Animated, { multiply } from "react-native-reanimated"
+import {useSpringTransition} from "react-native-redash"
+
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'red',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  outer: {
+    height: 200,
+    width: 200,
+    backgroundColor: 'black',
+    overflow: 'hidden'
+  },
+  inner: {
+    ...StyleSheet.absoluteFillObject,
+    left: -300,
+    width: 400,
+    height: 200,
+    backgroundColor: 'yellow'
+  }
+})
+
+export const PeekScroll = () => {
+  const [ left, setLeft ] = useState<0 | 1>(0)
+
+  const transitionSlide = multiply(useSpringTransition(left, {}), 400)
+
+  return (
+    <View style={styles.page}>
+      <View style={styles.outer}>
+        <Animated.View style={[styles.inner, {
+            transform: [
+              { translateX: transitionSlide },
+            ],
+          }]}>
+        </Animated.View>
+      </View>
+      <Button title='test' onPress={(): void => { left === 0 ? setLeft(1) : setLeft(0) }} />
+    </View>
+  );
+};
